661bc3f7f0172e71b430af73914fd875e67942f9
felixchua3143 <felixchua3142@gmail.com>
2024-07-28 23:57:13 +0800

    s

--- Scripts/packageList.js
+++ Scripts/packageList.js
@@ -2,4 +2,22 @@ const keyName = "CART";
 
 let cartItemsRaw = localStorage.getItem(keyName);
 
-// document.getElementById("packageList").innerHTML = cartItemsRaw;
+if(cartItemsRaw.id == 0){
+    document.getElementById("packageList").innerHTML =
+        '\
+    <section id="packageList"> \
+                <div id="packageTitle"> \
+                    <p> Your Chosen Package: </p> \
+                </div> \
+                <div id="packageImgDiv"> \
+                    <img id="packageImg" src="/Assignment 1/Media/pexels-emma-bauso-1183828-2253870.jpg"> \
+                </div> \
+                <div id="packageName"> \
+                    <p> Package 1 </p> \
+                </div> \
+                <div id="packagePrice"> \
+                    <p> $1500 </p> \
+                </div> \
+            </section > \
+'
+}
